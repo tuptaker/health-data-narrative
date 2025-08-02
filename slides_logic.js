f9833f5ce8bdfcf4bcb8f0fcde66fb3f071aec9e
Fabrizio <fabriziomachado@fastmail.com>
2025-08-02 17:12:26 -0400

    annotations for workout slide

--- slides_logic.js
+++ slides_logic.js
@@ -20,6 +20,251 @@ function initSlide0() {
     let pieDataRaw, stackedDataRaw, keys;
     const tooltip = d3.select(".tooltip");
 
+    let pieAnnotationsVisible = true;
+
+    function toggleAnnotations(forceHide = false) {
+        pieAnnotationsVisible = forceHide ? false : !pieAnnotationsVisible;
+        d3.selectAll(".annotation-group")
+            .style("display", pieAnnotationsVisible ? "block" : "none");
+        d3.select("#annotationToggleBtn")
+            .text(pieAnnotationsVisible ? "Hide Annotations" : "Show Annotations");
+    }
+
+    const chartsContainer = document.querySelector(".charts-container");
+    if (chartsContainer && !document.getElementById("annotationToggleBtn")) {
+        const button = document.createElement("button");
+        button.id = "annotationToggleBtn";
+        button.innerText = "Hide Annotations";
+        button.style.position = "absolute";
+        button.style.top = "10px";
+        button.style.right = "10px";
+        button.style.zIndex = "10";
+        button.onclick = () => toggleAnnotations();
+
+        chartsContainer.style.position = "relative";
+        chartsContainer.appendChild(button);
+    }
+
+    const updateCharts = () => {
+        d3.select("#donutChart").selectAll("*").remove();
+        d3.select("#stackedBarChart").selectAll("*").remove();
+
+        // Define arrow marker
+        d3.select("svg defs").remove();
+        const defs = d3.select("#donutChart").append("defs");
+        defs.append("marker")
+            .attr("id", "arrow")
+            .attr("viewBox", "0 -5 10 10")
+            .attr("refX", 5)
+            .attr("refY", 0)
+            .attr("markerWidth", 4)
+            .attr("markerHeight", 4)
+            .attr("orient", "auto")
+            .append("path")
+            .attr("d", "M0,-5L10,0L0,5")
+            .attr("fill", "black");
+
+        const filteredPie = pieDataRaw.filter(d => !disabledTypes.has(d.label));
+        const pie = d3.pie().value(d => d.value)(filteredPie);
+        const arc = d3.arc().innerRadius(100).outerRadius(180);
+        const svgPie = d3.select("#donutChart")
+            .append("g")
+            .attr("transform", "translate(200,200)");
+
+        svgPie.selectAll("path")
+            .data(pie)
+            .enter()
+            .append("path")
+            .attr("d", arc)
+            .attr("fill", d => colorMap[d.data.label])
+            .on("mousemove", (event, d) => {
+                const total = d3.sum(filteredPie, d => d.value);
+                const percent = ((d.data.value / total) * 100).toFixed(1);
+                tooltip
+                    .style("opacity", 1)
+                    .style("left", (event.pageX + 10) + "px")
+                    .style("top", (event.pageY - 28) + "px")
+                    .html(`<strong>${d.data.label}</strong><br/>${percent}%`);
+            })
+            .on("mouseout", () => tooltip.style("opacity", 0));
+
+        // Custom pie annotation (elbowed arrow to top-left)
+        const pieGroup = svgPie.append("g").attr("class", "annotation-group");
+
+        pieGroup.append("path")
+            .attr("d", "M 0 0 L -60 -100 L -140 -132")
+            .attr("stroke", "black")
+            .attr("fill", "none")
+            .attr("marker-end", "url(#arrow)");
+
+        pieGroup.append("rect")
+            .attr("x", -200)
+            .attr("y", -200)
+            .attr("width", 172)
+            .attr("height", 66)
+            .attr("rx", 8)
+            .attr("ry", 8)
+            .attr("fill", "#f9f9f9")
+            .attr("stroke", "black");
+
+        pieGroup.append("text")
+            .attr("x", -194)
+            .attr("y", -178)
+            .text("Different kinds of workouts as")
+            .style("font-size", "12px");
+
+        pieGroup.append("text")
+            .attr("x", -194)
+            .attr("y", -162)
+            .text("a percentage of all workout")
+            .style("font-size", "12px");
+
+        pieGroup.append("text")
+            .attr("x", -194)
+            .attr("y", -148)
+            .text("sessions since 2017.")
+            .style("font-size", "12px");
+
+        const filteredKeys = keys.filter(k => !disabledTypes.has(k));
+        const stackedData = d3.stack().keys(filteredKeys)(stackedDataRaw);
+
+        const margin = { top: 20, right: 20, bottom: 30, left: 40 },
+            width = 600 - margin.left - margin.right,
+            height = 400 - margin.top - margin.bottom;
+
+        const svgBar = d3.select("#stackedBarChart")
+            .append("g")
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+
+        const x = d3.scaleBand()
+            .domain(stackedDataRaw.map(d => d.year))
+            .range([0, width])
+            .padding(0.1);
+
+        const y = d3.scaleLinear()
+            .domain([0, d3.max(stackedDataRaw, d => d3.sum(filteredKeys, k => +d[k]))])
+            .nice()
+            .range([height, 0]);
+
+        svgBar.selectAll("g")
+            .data(stackedData)
+            .enter().append("g")
+            .attr("fill", d => colorMap[d.key])
+            .selectAll("rect")
+            .data(d => d)
+            .enter().append("rect")
+            .attr("x", d => x(d.data.year))
+            .attr("y", d => y(d[1]))
+            .attr("height", d => y(d[0]) - y(d[1]))
+            .attr("width", x.bandwidth())
+            .on("mousemove", (event, d) => {
+                const key = d3.select(event.target.parentNode).datum().key;
+                tooltip
+                    .style("opacity", 1)
+                    .style("left", (event.pageX + 10) + "px")
+                    .style("top", (event.pageY - 28) + "px")
+                    .html(`<strong>${key}</strong><br/>Count: ${d.data[key]}`);
+            })
+            .on("mouseout", () => tooltip.style("opacity", 0));
+
+        svgBar.append("g")
+            .attr("transform", `translate(0,${height})`)
+            .call(d3.axisBottom(x));
+
+        svgBar.append("g").call(d3.axisLeft(y));
+
+        // Custom bar annotation (elbowed arrow to top-right)
+        const barGroup = svgBar.append("g").attr("class", "annotation-group");
+
+        barGroup.append("path")
+            .attr("d", "M 320 100 L 360 20 L 400 10")
+            .attr("stroke", "black")
+            .attr("fill", "none")
+            .attr("marker-end", "url(#arrow)");
+
+        barGroup.append("rect")
+            .attr("x", 402)
+            .attr("y", -5)
+            .attr("width", 115)
+            .attr("height", 50)
+            .attr("rx", 8)
+            .attr("ry", 8)
+            .attr("fill", "#f9f9f9")
+            .attr("stroke", "black");
+
+        barGroup.append("text")
+            .attr("x", 408)
+            .attr("y", 16)
+            .text("Total workouts by")
+            .style("font-size", "12px");
+
+        barGroup.append("text")
+            .attr("x", 408)
+            .attr("y", 32)
+            .text("type per year.")
+            .style("font-size", "12px");
+
+        if (!pieAnnotationsVisible) toggleAnnotations(true);
+    };
+
+    Promise.all([
+        d3.csv("./data/breakout_by_workout_type.csv"),
+        d3.csv("./data/activity_summary_by_year.csv")
+    ]).then(([pieCSV, barCSV]) => {
+        pieDataRaw = pieCSV.map(d => ({
+            label: d.workoutActivityType,
+            value: +d.count
+        }));
+
+        stackedDataRaw = barCSV.map(d => {
+            d.year = d.year;
+            for (const key in d) {
+                if (key !== "year") d[key] = +d[key];
+            }
+            return d;
+        });
+
+        keys = barCSV.columns.slice(1);
+
+        const legend = d3.select("#legend");
+        keys.forEach(key => {
+            const item = legend.append("div")
+                .attr("class", "legend-item")
+                .attr("data-key", key);
+
+            item.append("div")
+                .attr("class", "legend-color")
+                .style("background-color", colorMap[key]);
+
+            item.append("div").text(key);
+
+            item.on("click", function() {
+                if (disabledTypes.has(key)) {
+                    disabledTypes.delete(key);
+                    d3.select(this).classed("disabled", false);
+                } else {
+                    disabledTypes.add(key);
+                    d3.select(this).classed("disabled", true);
+                }
+                toggleAnnotations(true);
+                updateCharts();
+            });
+        });
+
+        updateCharts();
+    });
+}
+
+
+function initSlide0Old() {
+    d3.select("#donutChart").selectAll("*").remove();
+    d3.select("#stackedBarChart").selectAll("*").remove();
+    d3.select("#legend").selectAll("*").remove();
+
+    const disabledTypes = new Set();
+    let pieDataRaw, stackedDataRaw, keys;
+    const tooltip = d3.select(".tooltip");
+
     const updateCharts = () => {
         d3.select("#donutChart").selectAll("*").remove();
         d3.select("#stackedBarChart").selectAll("*").remove();
